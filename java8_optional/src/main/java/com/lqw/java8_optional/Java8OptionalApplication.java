73a75db48f5897edd0836d0096189daac2d91cdd
liuquanwei <1353965910@qq.com>
2019-01-25 10:42:48 +0800

    Java8新特性 Optional

---
+++ java8_optional/src/main/java/com/lqw/java8_optional/Java8OptionalApplication.java
@@ -0,0 +1,14 @@
+package com.lqw.java8_optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8OptionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8OptionalApplication.class, args);
+    }
+
+}
+
---
+++ java8_optional/src/test/java/com/lqw/java8_optional/Java8OptionalApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lqw.java8_optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Java8OptionalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
